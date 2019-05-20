b4e97dfbd0d45daf0158a8da8d11aeb00814769e
研发二部 朱细平 <julian.zhu@longsys.com>
2019-05-20 18:23:16 +0800

    add go scan

---
+++ main.go
@@ -0,0 +1,35 @@
+// 19 august 2018
+
+// +build amd64 386
+
+package main
+
+// TODO probably a bug in libui: changing the font away from skia leads to a crash
+
+import (
+	// "errors"
+	"fmt"
+
+	//	"github.com/tdewolff/parse/html"
+	"github.com/goscan/ast"
+	"github.com/goscan/parser"
+
+	// "github.com/goscan/scanner"
+	"github.com/goscan/token"
+)
+
+func main() {
+
+	const src = `package main
+type x int // comment test
+`
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, "", src, parser.ParseComments)
+	if err != nil {
+		fmt.Println("parser failed")
+	}
+	comment := f.Decls[0].(*ast.GenDecl).Specs[0].(*ast.TypeSpec).Comment.List[0].Text
+	if comment != "// comment" {
+		fmt.Printf("got %q, want %q", comment, "// comment")
+	}
+}
